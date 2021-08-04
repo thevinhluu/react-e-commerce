23b35648e9e614d94092b8fe9bc10d3c5d28f81c
Luu The Vinh <luuthevinh1988@gmail.com>
2021-08-04 17:01:54 +0700

    react router setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
-import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import { Navbar, Sidebar, Footer } from './components'
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Navbar, Sidebar, Footer } from './components';
+
+import { Home, Products, SingleProduct, About, Cart, Error, Checkout, PrivateRoute } from './pages';
 
 function App() {
-  return <h4>comfy sloth starter</h4>
+	return (
+		<Router>
+			<Navbar />
+			<Sidebar />
+			<Switch>
+				<Route exact path='/'>
+					<Home />
+				</Route>
+				<Route path='/about'>
+					<About />
+				</Route>
+				<Route path='/cart'>
+					<Cart />
+				</Route>
+				<Route exact path='/products'>
+					<Products />
+				</Route>
+				<Route path='/products/:id' children={<SingleProduct />} />
+				<Route path='/checkout'>
+					<Checkout />
+				</Route>
+				<Route path='*'>
+					<Error />
+				</Route>
+			</Switch>
+			<Footer />
+		</Router>
+	);
 }
 
-export default App
+export default App;
