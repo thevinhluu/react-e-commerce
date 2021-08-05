f5bfc44c3d1897399d4133fb9acae966d0f418e7
Luu The Vinh <luuthevinh1988@gmail.com>
2021-08-05 20:16:19 +0700

    single product - stars

--- src/components/Stars.js
+++ src/components/Stars.js
@@ -1,22 +1,44 @@
-import React from 'react'
-import styled from 'styled-components'
-import { BsStarFill, BsStarHalf, BsStar } from 'react-icons/bs'
-const Stars = () => {
-  return <h4>stars</h4>
-}
+import React from 'react';
+import styled from 'styled-components';
+import { BsStarFill, BsStarHalf, BsStar } from 'react-icons/bs';
+
+const Stars = ({ stars, reviews }) => {
+	return (
+		<Wrapper>
+			<div className='stars'>
+				{/* stars */}
+				<span>{stars >= 1 ? <BsStarFill /> : stars >= 0.5 ? <BsStarHalf /> : <BsStar />}</span>
+				{/* end of stars */}
+				{/* stars */}
+				<span>{stars >= 2 ? <BsStarFill /> : stars >= 1.5 ? <BsStarHalf /> : <BsStar />}</span>
+				{/* end of stars */}
+				{/* stars */}
+				<span>{stars >= 3 ? <BsStarFill /> : stars >= 2.5 ? <BsStarHalf /> : <BsStar />}</span>
+				{/* end of stars */}
+				{/* stars */}
+				<span>{stars >= 4 ? <BsStarFill /> : stars >= 3.5 ? <BsStarHalf /> : <BsStar />}</span>
+				{/* end of stars */}
+				{/* stars */}
+				<span>{stars === 5 ? <BsStarFill /> : stars >= 4.5 ? <BsStarHalf /> : <BsStar />}</span>
+				{/* end of stars */}
+			</div>
+			<p className='reviews'>({reviews} customer reviews)</p>
+		</Wrapper>
+	);
+};
 
 const Wrapper = styled.div`
-  display: flex;
-  align-items: center;
-  span {
-    color: #ffb900;
-    font-size: 1rem;
-    margin-right: 0.25rem;
-  }
-  p {
-    margin-left: 0.5rem;
-    margin-bottom: 0;
-  }
-  margin-bottom: 0.5rem;
-`
-export default Stars
+	display: flex;
+	align-items: center;
+	span {
+		color: #ffb900;
+		font-size: 1rem;
+		margin-right: 0.25rem;
+	}
+	p {
+		margin-left: 0.5rem;
+		margin-bottom: 0;
+	}
+	margin-bottom: 0.5rem;
+`;
+export default Stars;
