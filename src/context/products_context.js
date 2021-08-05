ba6441567d6ce0dde4f2f8ecd69facdd72d7e468
Luu The Vinh <luuthevinh1988@gmail.com>
2021-08-05 15:03:33 +0700

    fetch products complete

--- src/context/products_context.js
+++ src/context/products_context.js
@@ -14,7 +14,11 @@ import {
 } from '../actions';
 
 const initialState = {
-	isSidebarOpen : false
+	isSidebarOpen     : false,
+	products_loading  : false,
+	products_error    : false,
+	products          : [],
+	featured_products : []
 };
 
 const ProductsContext = React.createContext();
@@ -30,8 +34,14 @@ export const ProductsProvider = ({ children }) => {
 	};
 
 	const fetchProducts = async (url) => {
-		const response = await axios.get(url);
-		console.log(response);
+		dispatch({ type: GET_PRODUCTS_BEGIN });
+		try {
+			const response = await axios.get(url);
+			const products = response.data;
+			dispatch({ type: GET_PRODUCTS_SUCCESS, payload: products });
+		} catch (error) {
+			dispatch({ type: GET_PRODUCTS_ERROR });
+		}
 	};
 
 	useEffect(() => {
