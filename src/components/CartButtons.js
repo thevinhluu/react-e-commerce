ee98c89c1f369ee8a6be10be5e603611efabea61
Luu The Vinh <luuthevinh1988@gmail.com>
2021-08-07 00:07:52 +0700

    auth0 - hide checkout

--- src/components/CartButtons.js
+++ src/components/CartButtons.js
@@ -8,7 +8,7 @@ import { useUserContext } from '../context/user_context';
 
 const CartButtons = () => {
 	const { closeSidebar } = useProductsContext();
-	const { total_item } = useCartContext();
+	const { total_items } = useCartContext();
 	const { loginWithRedirect, logout, myUser } = useUserContext();
 	return (
 		<Wrapper className='cart-btn-wrapper'>
@@ -16,7 +16,7 @@ const CartButtons = () => {
 				Cart
 				<span className='cart-container'>
 					<FaShoppingCart />
-					<span className='cart-value'>{total_item}</span>
+					<span className='cart-value'>{total_items}</span>
 				</span>
 			</Link>
 			{myUser ? (
