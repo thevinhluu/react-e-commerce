0322f7352f7cf983f17982d344eb1af88737676f
Luu The Vinh <luuthevinh1988@gmail.com>
2021-08-06 14:55:10 +0700

    filters - shipping and clear filters

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -107,7 +107,16 @@ const Filters = () => {
 						<input type='range' name='price' min={min_price} max={max_price} value={price} onChange={updateFilters} />
 					</div>
 					{/* end of price */}
+					{/* shipping */}
+					<div className='form-control shipping'>
+						<label htmlFor='shipping'>free shipping</label>
+						<input type='checkbox' name='shipping' id='shipping' onChange={updateFilters} checked={shipping} />
+					</div>
+					{/* end of shipping */}
 				</form>
+				<button type='button' className='clear-btn' onClick={clearFilters}>
+					clear filters
+				</button>
 			</div>
 		</Wrapper>
 	);
