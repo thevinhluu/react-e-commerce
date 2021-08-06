030a93a8a37ede123bef98617cdb0eadf00042aa
Luu The Vinh <luuthevinh1988@gmail.com>
2021-08-06 23:17:01 +0700

    userContext setup

--- src/index.js
+++ src/index.js
@@ -9,12 +9,21 @@ import { UserProvider } from './context/user_context';
 import { Auth0Provider } from '@auth0/auth0-react';
 
 ReactDOM.render(
-	<ProductsProvider>
-		<FilterProvider>
-			<CartProvider>
-				<App />
-			</CartProvider>
-		</FilterProvider>
-	</ProductsProvider>,
+	<Auth0Provider
+		domain='dev-jxzc2-th.us.auth0.com'
+		clientId='tvNvl6wbLyOaqks2G32XU8kAeF2fgu8l'
+		redirectUri={window.location.origin}
+		cacheLocation='localstorage'
+	>
+		<UserProvider>
+			<ProductsProvider>
+				<FilterProvider>
+					<CartProvider>
+						<App />
+					</CartProvider>
+				</FilterProvider>
+			</ProductsProvider>
+		</UserProvider>
+	</Auth0Provider>,
 	document.getElementById('root')
 );
