13363a326f5a6a58635b0cd37c51a57de49a8096
Luu The Vinh <luuthevinh1988@gmail.com>
2021-08-06 18:05:08 +0700

    localStorage

--- src/context/cart_context.js
+++ src/context/cart_context.js
@@ -2,8 +2,18 @@ import React, { useEffect, useContext, useReducer } from 'react';
 import reducer from '../reducers/cart_reducer';
 import { ADD_TO_CART, REMOVE_CART_ITEM, TOGGLE_CART_ITEM_AMOUNT, CLEAR_CART, COUNT_CART_TOTALS } from '../actions';
 
+const getLocalStorage = () => {
+	let cart = localStorage.getItem('cart');
+	if (cart) {
+		return JSON.parse(localStorage.getItem('cart'));
+	}
+	else {
+		return [];
+	}
+};
+
 const initialState = {
-	cart         : [],
+	cart         : getLocalStorage(),
 	total_items  : 0,
 	total_amount : 0,
 	shipping_fee : 534
@@ -24,6 +34,13 @@ export const CartProvider = ({ children }) => {
 	const toggleAmount = (id, value) => {};
 	// clear cart
 	const clearCart = () => {};
+
+	useEffect(
+		() => {
+			localStorage.setItem('cart', JSON.stringify(state.cart));
+		},
+		[state.cart]
+	);
 
 	return (
 		<CartContext.Provider value={{ ...state, addToCart, removeItem, toggleAmount, clearCart }}>
