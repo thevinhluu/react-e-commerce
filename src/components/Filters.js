90e66b8781ba59333670e315756a94b320b349cb
Luu The Vinh <luuthevinh1988@gmail.com>
2021-08-06 11:44:27 +0700

    filters - companies

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -52,6 +52,20 @@ const Filters = () => {
 						</div>
 					</div>
 					{/* end of categories */}
+					{/* companies */}
+					<div className='form-control'>
+						<h5>company</h5>
+						<select name='company' value={company} onChange={updateFilters} className='company'>
+							{companies.map((c, index) => {
+								return (
+									<option key={index} value={c}>
+										{c}
+									</option>
+								);
+							})}
+						</select>
+					</div>
+					{/* end of companies */}
 				</form>
 			</div>
 		</Wrapper>
