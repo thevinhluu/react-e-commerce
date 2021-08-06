ed8e64d05729a6d32d0828f655b320d6e510f029
Luu The Vinh <luuthevinh1988@gmail.com>
2021-08-06 12:44:05 +0700

    filters - price

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -100,6 +100,13 @@ const Filters = () => {
 						</div>
 					</div>
 					{/* end of colors */}
+					{/* price */}
+					<div className='form-control'>
+						<h5>price</h5>
+						<p className='price'>{formatPrice(price)}</p>
+						<input type='range' name='price' min={min_price} max={max_price} value={price} onChange={updateFilters} />
+					</div>
+					{/* end of price */}
 				</form>
 			</div>
 		</Wrapper>
